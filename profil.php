fe6e115ea1b8de8c90941d9cb1c38f93d5e35cf8
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2019-12-07 12:08:19 +0100

    fix sécurité edit profil

--- profil.php
+++ profil.php
@@ -9,24 +9,31 @@ if (isset($_GET["logout"])) {
     header("location:index.php");
 }
 $userconnected = $_SESSION["isconnected"];
+$connectstate = true;
 $login = mysqli_connect("localhost", "root", "", "livreor");
 $request = "SELECT * FROM utilisateurs WHERE login = '" . $userconnected . "'";
 $query = mysqli_query($login, $request);
 $result = mysqli_fetch_all($query);
 $pass = $result[0][2];
+$request2 = "SELECT id FROM utilisateurs WHERE login = '" . $_SESSION["isconnected"] . "'";
+$query2 = mysqli_query($login, $request2);
+$checklogin = mysqli_num_rows($query2);
 if (isset($_POST["submit"])) {
-    if ($_POST["password"] == $_POST["passwordconfirm"]) {
-        if (password_verify($_POST["password"], $pass)) {
-            $editrequest = "UPDATE utilisateurs SET login = '" . $_POST["login"] . "' WHERE login = '" . $userconnected . "'";
-            mysqli_query($login, $editrequest);
-            mysqli_close($login);
-            header("location:index.php");
-            $_SESSION["isconnected"] = $_POST["login"];
+    if ($checklogin < 1) {
+        if ($_POST["password"] == $_POST["passwordconfirm"]) {
+            if (password_verify($_POST["oldpassword"], $pass)) {
+                $editrequest = "UPDATE utilisateurs SET login = '" . $_POST["login"] . "', password = '" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "' WHERE login = '" . $userconnected . "'";
+                mysqli_query($login, $editrequest);
+                mysqli_close($login);
+                header("location:index.php");
+                $_SESSION["isconnected"] = $_POST["login"];
+            }
         } else {
             echo "ERROR hash";
+            $connectstate = false;
         }
     } else {
-        echo "ERROR";
+        $connectstate = false;
     }
 }
 ?>
@@ -69,17 +76,23 @@ if (isset($_POST["submit"])) {
     <main>
         <div id="alignlogin" class="flexc">
             <section id="loginbox" class="flexc center">
-                <p id="regtxt" class="center">Veuillez renseignez les champ pour modifier votre profil</p>
+                <p id="regtxt" class="center">Veuillez renseignez les champ pour modifier vos informations</p>
                 <form class="flexc center" action="profil.php" method="POST">
                     <label for="login">Login</label>
                     <input class="login_input" type="text" name="login" placeholder=<?php foreach ($result as $row) {
-                                                                                        echo $row[1];
-                                                                                    } ?> required>
-                    <label for="password">Password</label>
+                                                                                    echo $row[1];
+                                                                                } ?> required>
+                    <label for="oldpassword">Ancient Password</label>
+                    <input class="login_input" type="password" name="oldpassword" placeholder="******">
+                    <label for="password">Nouveau Password</label>
                     <input class="login_input" type="password" name="password" placeholder="******">
                     <label for="passwordconfirm">Confirmez le password</label>
                     <input class="login_input" type="password" name="passwordconfirm" placeholder="******">
                     <input id="index_button" class="center" name="submit" type="submit" value="Valider">
+                    <?php if ($connectstate == false) {
+                        echo "<p class='footertxt'>Login existant</p>";
+                    }
+                    ?>
                 </form>
             </section>
         </div>
